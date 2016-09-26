78f9e5d2d65630aefbb8a52278b4edacf6891756
wilsoncarvajal <wilsoncarvaja@gmail.com>
2016-09-25 21:49:51 -0300

    Funcionalidad lectura Json Mejorada
    Funcionalidad lectura Json Mejorada

--- noticiaApp.js
+++ noticiaApp.js
@@ -1,27 +1,25 @@
 /**
  * Created by Wizao on 23-09-2016.
+ * modified by Jota on 25-09-2016.
  */
 var app = angular.module('noticiaApp', []);
 
 app.controller('mainController', function($scope){
 
     $scope.noticias = [] ;
-    $scope.noticiaJson = '';
+    $scope.i = 0;
 
-    $scope.transformarNoticia = function(){
 
-        $scope.newPost.created_at = Date.now();
-        $scope.posts.push($scope.newPost);
-        $scope.newPost = {created_by: '', text: '', created_at: ''};
-
-    };
 
     $scope.transformarJson = function(){
 
         $scope.noticiaObjeto  = angular.fromJson($scope.noticiaJson);
-        $scope.noticias.push($scope.noticiaObjeto);
-        $scope.noticiaJson = '';
+
+        for( $scope.i in  $scope.noticiaObjeto){
+            $scope.noticias.push($scope.noticiaObjeto[$scope.i]);
+        };
 
 
     };
+
 });
