a65a895f9569c2fa343f67d6f15e732cfce02688
wilsoncarvajal <wilsoncarvaja@gmail.com>
2016-09-25 23:58:56 -0300

    Arreglos de errores del Commit Anterior

--- noticiaApp.js
+++ noticiaApp.js
@@ -8,16 +8,9 @@ app.controller('mainController', function($scope){
 
     $scope.noticias = [] ;
     $scope.i = 0;
-    //$scope.noticiaJson = '';
 
-    $scope.transformarNoticia = function(){
 
-        $scope.newPost.created_at = Date.now();
-        $scope.posts.push($scope.newPost);
-        $scope.newPost = {created_by: '', text: '', created_at: ''};
-
-    };
-
+    
     $scope.transformarJson = function(){
 
         $scope.noticiaObjeto  = angular.fromJson($scope.noticiaJson);
