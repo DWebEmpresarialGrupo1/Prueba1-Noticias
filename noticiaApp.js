8bf5768e68269dcee173d13fbb54b0c29567e442
Juan Pizarro <jottapizarro@gmail.com>
2016-09-27 10:09:34 -0400

    Cambio en noticiaApp.js
    se cargan las noticias correspondientes en la lista y se borran cuando
    se saca alguna del json.

--- noticiaApp.js
+++ noticiaApp.js
@@ -6,22 +6,20 @@ var app = angular.module('noticiaApp', []);
 
 app.controller('mainController', function($scope){
 
-    $scope.noticias = [] ;
-    //$scope.noticiaJson = '';
+    $scope.i = 0;
 
-    $scope.transformarNoticia = function(){
 
-        $scope.newPost.created_at = Date.now();
-        $scope.posts.push($scope.newPost);
-        $scope.newPost = {created_by: '', text: '', created_at: ''};
-
-    };
-
+    
     $scope.transformarJson = function(){
 
+		$scope.noticias = [] ;
         $scope.noticiaObjeto  = angular.fromJson($scope.noticiaJson);
-        $scope.noticias.push($scope.noticiaObjeto);
-		//$scope.noticiaJson = '';
+
+        for( $scope.i in  $scope.noticiaObjeto){
+            $scope.noticias.push($scope.noticiaObjeto[$scope.i]);
+        };
+
+
     };
 
 });
